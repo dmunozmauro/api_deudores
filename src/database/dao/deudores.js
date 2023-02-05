995e31e42c22d072d825c9a8cd250f0171264e79
dmunoz <dmunoz@aligare.cl>
2023-02-05 15:05:07 -0300

    prueba

--- src/database/dao/deudores.js
+++ src/database/dao/deudores.js
@@ -2,13 +2,13 @@ import { sequel } from ".."
 import { QueryTypes } from 'sequelize'
 
 export const valida_deudor = async (deudor) => {
-    let query = `select td.deudor from tal_deudores td where td.deudor = '${deudor}'`
+    let query = `select td.deudor from tal_deudores td where td.deudor = '${deudor}'`;
 
     return await sequel.query(query, { type: QueryTypes.SELECT });
 }
 
 export const insertar_deudores = async (deudor, t) => {
-    let query = `insert into tal_deudores(deudor) values($1)`
+    let query = `insert into tal_deudores(deudor) values($1)`;
 
     return await sequel.query(query, {
         type: QueryTypes.INSERT,
